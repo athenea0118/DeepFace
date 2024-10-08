ddfe526469fe080c3445eee852be1d49dd2f53c0
athenea0118 <breakAI0118@gmail.com>
2024-10-08 11:31:19 -0400

    server base version

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import records from './routes/record';
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.join());
+app.use('/record', records);
+
+//record/12345
 
 
+//Start the Express server
 app.listen(PORT, () => {
-    console.log('Server is listening on port ${PORT}');
+    console.log(`Server is listening on port ${PORT}`);
 });
